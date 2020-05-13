4dabe5f429b35f4980d7d3f8ca731cb3a60fa77b
Dschoni <jonathan.schock@tum.de>
2020-05-13 14:59:01 +0200

    Fully qualify imports

--- imreg/sampler.py
+++ imreg/sampler.py
@@ -2,14 +2,7 @@
 
 import numpy as np
 import scipy.ndimage as nd
-
-try:
-    import interpolation
-except ImportError as error:
-    # Attempt autocompilation.
-    import pyximport
-    pyximport.install()
-    import _interpolation as interpolation
+import imreg.interpolation as interpolation
 
 
 def nearest(image, warp):
